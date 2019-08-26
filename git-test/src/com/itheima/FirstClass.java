b26d21984715b4f5a17bd6632cd17a60f2d5d1a1
Ghh <1191336892@qq.com>
2019-08-26 17:30:46 +0800

    克隆下来的工程进行的第一次修改

--- git-test/src/com/itheima/FirstClass.java
+++ git-test/src/com/itheima/FirstClass.java
@@ -7,5 +7,8 @@ public class FirstClass {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+
+        System.out.println("hello git5");
+        System.out.println("hello git6");
     }
 }
